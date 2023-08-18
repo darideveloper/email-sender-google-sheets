983fc9bb2ee8640e40cd7a3df338c61973d31b36
daridev <darideveloper@gmail.com>
2023-08-18 17:30:29 -0600

    feat (main): submit html email

--- __main__.py
+++ __main__.py
@@ -1,12 +1,43 @@
 import os
 from dotenv import load_dotenv
-from logs import logger
-from scraping_manager.automate import WebScraping
+from email_manager.sender import EmailManager
+from spreadsheets.google_sheets import SheetsManager
 
 load_dotenv ()
-VAR = os.getenv ("VAR")
+EMAIL_HOST_USER = os.getenv ("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv ("EMAIL_HOST_PASSWORD")
+EMAIL_SUBJECT = os.getenv ("EMAIL_SUBJECT")
+GOOGLE_SHEETS = os.getenv ("GOOGLE_SHEETS")
+
+current_folder = os.path.dirname (__file__)
+creds_path = os.path.join (current_folder, "credentials.json")
+html_path = os.path.join (current_folder, "template.html")
 
 def main (): 
+    
+    # Connect to email
+    email_manager = EmailManager (EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
+    
+    # Get emails from google sheets
+    sheets_manager = SheetsManager (GOOGLE_SHEETS, creds_path)
+    sheets_manager.set_sheet ("contact form")
+    data = sheets_manager.get_data ()
+    
+    for row in data:
+    
+        # Submit email
+        email_manager.send_email (
+            receivers=[row["email"]],
+            subject=EMAIL_SUBJECT,
+            html_path=html_path
+        )
+        
+        # Update date in google sheets
+        print ()
+        
+        # Update status in google sheets
+        
+    pass
 
 
 if __name__ == "__main__":
